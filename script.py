0b830dbc513bdc36f001b261e6b8b29f04592034
Ivan073 <vanya_zhechko@tut.by>
2023-02-23 02:47:04 +0300

    script2

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
 print("Hello World")
+
+inp1=int(input("Number 1: "))
+inp2=int(input("Number 2: "))
+op=input("Operation: ")
+
+if op=="add":
+    print(inp1+inp2)
+elif op=="sub":
+    print(inp1-inp2)
+elif op=="mult":
+    print(inp1*inp2)
+elif op=="div":
+    print(inp1/inp2)
+
