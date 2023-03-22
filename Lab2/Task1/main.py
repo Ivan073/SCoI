e1f2dc1ebb3eaaefcbd658139169f5d986981b89
Ivan073 <vanya_zhechko@tut.by>
2023-03-22 22:12:35 +0300

    Added input for text and related functions

---
+++ Lab2/Task1/main.py
@@ -0,0 +1,13 @@
+from Task1.input import get_text
+
+
+def main():
+    print("Choose the way to get text:\n" +
+          "1. Input text\n" +
+          "2. Input path to file")
+    text = get_text()
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
