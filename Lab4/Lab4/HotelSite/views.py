907fab62503856100424be0538d196ea09972bb4
Ivan073 <vanya_zhechko@tut.by>
2023-08-27 20:19:27 +0300

    Added first page

--- Lab4/Lab4/HotelSite/views.py
+++ Lab4/Lab4/HotelSite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test_view(request):
-    return HttpResponse("test23")
+def login_view(request):
+    return render(request, "login.html")
 
 # Create your views here.
