26f39b81efa876df2b85977705cf828fbb54de36
Ivan073 <vanya_zhechko@tut.by>
2023-08-27 12:23:42 +0300

    view test change 2

--- Lab4/Lab4/HotelSite/views.py
+++ Lab4/Lab4/HotelSite/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test_view(request):
-    return HttpResponse("test")
+    return HttpResponse("test2")
 
 # Create your views here.
