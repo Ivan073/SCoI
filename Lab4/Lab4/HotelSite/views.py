93983208286341da4da3a87fb924fc30a9351c71
Ivan073 <vanya_zhechko@tut.by>
2023-08-27 20:41:56 +0300

    Login page placeholder

--- Lab4/Lab4/HotelSite/views.py
+++ Lab4/Lab4/HotelSite/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            #return redirect(views.home)
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
 
 # Create your views here.
