48020d0b179eaea62fcf2aa40785d1f88d0b5a7e
Ivan073 <vanya_zhechko@tut.by>
2023-08-28 13:54:19 +0300

    Home page placeholder with header and message in login

--- Lab4/Lab4/HotelSite/urls.py
+++ Lab4/Lab4/HotelSite/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('', views.home_view),
     path('login', views.login_view),
     path('signup', views.signup_view),
 ]
--- Lab4/Lab4/HotelSite/views.py
+++ Lab4/Lab4/HotelSite/views.py
@@ -4,15 +4,17 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_view(request):
+    context={"error":False}
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            #return redirect(views.home)
+            return redirect(views.home)
+        context={"error":True}
     else:
         form = AuthenticationForm()
-    return render(request, "login.html")
+    return render(request, "login.html", context)
 
 def signup_view(request):
     if request.method == "POST":
@@ -20,7 +22,13 @@ def signup_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            #return redirect(views.home)
+            return redirect(views.home)
     else:
         form = AuthenticationForm()
     return render(request, "signup.html")
+
+def home_view(request):
+    context={
+        "user":request.user
+    }
+    return render(request, "home.html",context=context)
