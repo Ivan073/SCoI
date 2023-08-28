40aba905f0c8574e5a4a2efa579de5d27ee60df7
Ivan073 <vanya_zhechko@tut.by>
2023-08-28 13:19:03 +0300

    More styles for login and sign up

--- Lab4/Lab4/HotelSite/views.py
+++ Lab4/Lab4/HotelSite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
 
+
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
