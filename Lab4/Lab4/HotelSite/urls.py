83d0be612f20c9210e900a76bf13eab770634fe2
Ivan073 <vanya_zhechko@tut.by>
2023-08-29 15:01:57 +0300

    Header to login and sign in

--- Lab4/Lab4/HotelSite/urls.py
+++ Lab4/Lab4/HotelSite/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home_view),
+    path('', views.home_view, name='home'),
     path('login', views.login_view),
     path('logout', views.logout_view),
     path('signup', views.signup_view),
