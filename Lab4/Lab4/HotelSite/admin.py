9cc846b3c8dc0ec384f4c3fa71e7b62fb77e9259
Ivan073 <vanya_zhechko@tut.by>
2023-08-30 16:36:53 +0300

    Created and added rest of models

--- Lab4/Lab4/HotelSite/admin.py
+++ Lab4/Lab4/HotelSite/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Client, ClientData
+from .models import Client, ClientData, Room, RoomType, Booking
 
 admin.site.register(Client)
 admin.site.register(ClientData)
-# Register your models here.
+admin.site.register(Room)
+admin.site.register(RoomType)
+admin.site.register(Booking)
--- Lab4/Lab4/HotelSite/models.py
+++ Lab4/Lab4/HotelSite/models.py
@@ -11,8 +11,25 @@ class Client(AbstractUser):
     username = None
     email = models.EmailField(unique=True, primary_key=True)
     patronymic = models.CharField(max_length=100, blank=True)
-    client_data = models.OneToOneField(ClientData, on_delete=models.CASCADE, blank=True)
+    client_data = models.OneToOneField(ClientData, on_delete=models.CASCADE, blank=True, related_name='client')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = ClientManager()
+
+
+class RoomType(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+
+class Room(models.Model):
+    description = models.CharField(max_length=400, blank=True)
+    photo = models.FileField(blank=True)
+    price = models.DecimalField(blank=True, decimal_places=2, max_digits=12)
+    capacity = models.IntegerField(blank=True)
+    room_type = models.OneToOneField(RoomType, on_delete=models.CASCADE, blank=True)
+
+class Booking(models.Model):
+    client = models.OneToOneField(Client, on_delete=models.CASCADE, blank=True, related_name='booking')
+    room = models.OneToOneField(Room, on_delete=models.CASCADE, blank=True, related_name='booking')
+    entry_date = models.DateTimeField(blank=True)
+    departure_date = models.DateTimeField(null=True,blank=True)
